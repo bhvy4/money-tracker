fc0da6b8fc71bddc548ec86d9c94784d71a8446c
bhvy4 <khanna.micky01@gmail.com>
2023-02-22 00:17:42 +0530

    auth state changes / route guarding / Home form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import { Switch, BrowserRouter, Route } from 'react-router-dom'
-import Navbar from './components/Navbar';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import Signup from './pages/signup/Signup';
+import { Switch, BrowserRouter, Route, Redirect } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-      <Navbar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/signup'>
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {user && <Home />}
+              {!user && <Redirect to="/login" />}
+            </Route>
+            <Route path="/login">
+              {user && <Redirect to="/" />}
+              {!user && <Login />}
+            </Route>
+            <Route path="/signup">
+              {user && <Redirect to="/" />}
+              {!user && <Signup />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,27 +1,37 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
+import { firebaseAuth } from "../firebase/config";
 
 export const AuthContext = createContext();
 
 const authReducer = (state, action) => {
-    switch (action.type) {
-        case 'LOGIN':
-            return { ...state, user: action.payload }
-        case 'LOGOUT':
-            return {...state, user: null}
-        default:
-            return state
-    }
-
-}
+  switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
+    case "LOGOUT":
+      return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state , user: action.payload, authIsReady: true}
+    default:
+      return state;
+  }
+};
 export const AuthContextProvider = ({ children }) => {
-
-    const [state, dispatch] = useReducer(authReducer, {
-        user: null
-    })
-    console.log('authcontext state : ',state)
-    return (
-        < AuthContext.Provider value={{ ...state, dispatch }}>
-            {children}
-        </AuthContext.Provider >
-    )
-}
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady : false
+  });
+  
+  useEffect(() => {
+      const unsub = firebaseAuth.onAuthStateChanged((user) => {
+          dispatch({type:'AUTH_IS_READY' , payload:user})
+          unsub()
+        });
+    }, []);
+    
+    console.log("authcontext state : ", state)
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
