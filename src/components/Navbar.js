89cdc819a0777d0ab5181c61e321a649c7786e6c
bhvy4 <khanna.micky01@gmail.com>
2023-02-21 18:02:02 +0530

    conditional showing user content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,39 @@
-import styles from './Navbar.module.css'
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
-
-    const {logout} = useLogout()
-    return (
-        <nav className={styles.navbar}>
-            <ul>
-                <li className={styles.title}><Link to='/'>Money Tracker</Link> </li>
-                <li><Link to='/login'>Login</Link> </li>
-                <li><Link to='/signup'>Signup</Link></li> 
-                <button className='btn'onClick={logout}> Logout</button>
-            </ul>
-        </nav>
-    )
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>
+          <Link to="/">Money Tracker</Link>{" "}
+        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>{" "}
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li> Hello, {user.displayName} </li>
+            <li>
+              <button className="btn" onClick={logout}>
+                {" "}
+                Logout
+              </button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
 }
